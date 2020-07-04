6e0ae0d8b23c5e97ff58915b50870db99413a922
pi-12-3 <pi12pi3@gmail.com>
2020-07-04 14:12:34 +0900

    Err処理を加えた

--- pyworks/dice.py
+++ pyworks/dice.py
@@ -1,6 +1,10 @@
 import random
 
 class Dice:
-    face_num=6
+    def __init__(self,val=6):
+        if val not in [4,6,8,12,20]:
+            raise Exception('そのような正多面体はありません')
+        self.face_num=val
+
     def shoot(self):
         return random.randint(1,self.face_num)
